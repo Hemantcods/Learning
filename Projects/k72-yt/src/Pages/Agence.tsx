c22d9123ea3a3d71d93c5a971eaee5848ba9f5ce
hemant <hy564636@gmail.com>
2026-06-03 12:53:21 +0530

    made the agence page

--- Projects/k72-yt/src/Pages/Agence.tsx
+++ Projects/k72-yt/src/Pages/Agence.tsx
@@ -1,14 +1,57 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import  { useRef } from "react";
 
 const Agence = () => {
+  const imageDivRef = useRef(null);
+  const imageRef=useRef(null)
+  const imageArray=[
+    'https://k72.ca/images/teamMembers/SebR_640X960.jpg?w=640&h=960&s=81dfdbd4b658503ba32862901a1ea3ca',
+    'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
+    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6'
+    
+  ]
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(function(){
+    gsap.to(imageDivRef.current,{
+      scrollTrigger:{
+        trigger:imageDivRef.current,
+        start:'top 20%',
+        end:'top -80%',
+        pin:true,
+        scrub:true,
+        onUpdate:function(elem){
+          let imageindex
+          if (elem.progress<1){
+            imageindex=Math.floor((elem.progress)* imageArray.length)
+          }else{
+            imageindex=imageArray.length-1
+          } 
+          console.log(imageindex)
+          imageRef.current.src=imageArray[imageindex]
+        }
+      }
+    })
+  })
   return (
     <div>
       <div className="section1">
-        <div className="absolute overflow-hidden w-[15vw] bg-red-500 h-[20vw] rounded-4xl top-[20vh] left-[30vw]">
+        <div
+          className="absolute overflow-hidden w-[15vw] h-[20vw] rounded-4xl top-[20vh] left-[30vw]"
+          ref={imageDivRef}
+        >
           <img
             className="h-full w-full object-cover"
             src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
             alt=""
+            ref={imageRef}
           />
         </div>
         <div className="relative font-[font2]">
@@ -30,9 +73,7 @@ const Agence = () => {
         </div>
       </div>
 
-      <div className="section2 h-screen">
-
-      </div>
+      <div className="section2 h-screen"></div>
     </div>
   );
 };
--- Projects/k72-yt/src/components/home/Video.tsx
+++ Projects/k72-yt/src/components/home/Video.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video autoPlay loop muted className='object-cover w-full h-full' src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1780466095~exp=1780469695~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=3acf3530786db43ba6eff707f2f5c8138f5d837def97f0531a73f05650407aa1&r=dXMtZWFzdDE%3D"></video>
+        <video autoPlay loop muted className='object-cover w-full h-full' src="/video/video.mp4"></video>
     </div>
   )
 }
