d5000bb56d93333a6efe0c78c436df94a7916a44
hemantcods <hy564636@gmail.com>
2026-07-16 21:48:53 +0530

    Add Chat-BE: initial websocket server setup

---
+++ Projects/Simple ChatApp using websockets/Chat-BE/src/index.ts
@@ -0,0 +1,46 @@
+import { WebSocket } from "ws";
+import { WebSocketServer } from "ws";
+
+interface CustomSocket extends WebSocket {
+  roomId?: string;
+}
+
+const rooms = new Map<string, Set<CustomSocket>>();
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (socket: CustomSocket) => {
+  console.log("User connected:");
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message.toString());
+
+    if (parsed.type === "join") {
+      const roomId = parsed.payload.roomId;
+
+      socket.roomId = roomId;
+
+      if (!rooms.has(roomId)) {
+        rooms.set(roomId, new Set());
+      }
+
+      rooms.get(roomId)!.add(socket);
+    }
+
+    if (parsed.type === "chat") {
+      const room = rooms.get(socket.roomId!);
+
+      if (!room) return;
+
+      room.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({
+              type: "chat",
+              payload: {
+                message: parsed.payload.message,
+              },
+            }),
+          );
+        }
+      });
+    }
+  });
+});
